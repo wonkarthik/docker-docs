b1246771ec84aa34a7009c5671327b3245cf9e93
won karthik <wonkarthik@gmail.com>
2019-09-07 15:38:49 +0000

    update theme options

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,7 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'logo_only': True
 }
+
 html_theme_path = ["../.."]
 html_logo = "demo/static/logo-wordmark-light.svg"
 html_show_sourcelink = True
